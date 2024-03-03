ec5c34b05e7d84538c9f4b43a609a709db2bea39
Paninap <92017318+ssk-Paninap@users.noreply.github.com>
2024-03-03 23:34:04 +0800

    Update statsMedian.py

--- statsMedian.py
+++ statsMedian.py
@@ -1,7 +1,10 @@
 import statistics
-
+#function to get median of a given (will be converted to list if it is a tuple)
 def getMedian(a):
+    #return the median
     return statistics.median(list(a))
+#Example
 myTupols = (21,32,45,65,23,65,23)
 x = getMedian(myTupols)
+#displaying the Median
 print(f"{x} is the Median" )   
