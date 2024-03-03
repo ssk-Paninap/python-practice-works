a2e41e8823b41f7e2baf1c7071fe55a597af918e
Paninap <92017318+ssk-Paninap@users.noreply.github.com>
2024-03-04 00:35:44 +0800

    Update mathModules_review.py

--- mathModules_review.py
+++ mathModules_review.py
@@ -2,7 +2,10 @@ import math
 import statistics
 
 numCont =[]
-
+"""
+1. Created a loop where in it will continouly ask user for values until user ended it
+2. Appends all the values entered by user to the numCont list and uses floor to remove the decimal
+"""
 while True:
     userInput = float(input("Input values for the list: "))
     
@@ -13,6 +16,11 @@ while True:
     
     if askQ != 'y':
         break
+"""
+Getting the median with the help of statistic library and getting sum of all the values 
+entered by user. 
+displaying the output of all three (entered values, median, and sum)
+"""
 getMed = statistics.median(numCont)
 total = sum(numCont)
 print(f"the sum of the values {numCont} is {total} and median is {getMed}")
