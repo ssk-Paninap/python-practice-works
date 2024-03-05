48070ab44bb5fcfe089cb3e98999c3c17ce1a925
Paninap <92017318+ssk-Paninap@users.noreply.github.com>
2024-03-05 10:06:13 +0800

    Update str_init_class.py

--- str_init_class.py
+++ str_init_class.py
@@ -1,7 +1,13 @@
+'''
+class named Arithmetic which has the functions for 
+                        addition,subtraction and multiplication
+'''
 class Arithmetic:
+    #__init__ means initialize (we can initialize variables here)
     def __init__(self,n1,n2):
         self.n1 = n1
         self.n2 = n2
+    #__str__ is a dunder method which shows a string representation of what will be shown 
     def __str__(self):
         return f"{self.n1} ({self.n2})"
     
